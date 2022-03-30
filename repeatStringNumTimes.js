469a69d03db1979e5727804660186f2f0719e903
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-03-30 22:05:03 +0100

    Solve repeatStringNumTimes with for loop

--- repeatStringNumTimes.js
+++ repeatStringNumTimes.js
@@ -8,3 +8,13 @@ function repeatStringNumTimes(str, num) {
 }
 
 console.log(repeatStringNumTimes('abc', 3));
+
+function repeatStringNumTimes2(str, num) {
+  let repeatStr = '';
+  for (let i = 0; i < num; i++) {
+    repeatStr += str;
+  }
+  return repeatStr;
+}
+
+console.log(repeatStringNumTimes2('bad', 3));
