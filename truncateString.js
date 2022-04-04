c6c1670bf8e7979ab65a4091ea59b88d07cbcb48
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-04 08:20:03 +0100

    Solve truncate string challenge

---
+++ truncateString.js
@@ -0,0 +1,12 @@
+function truncateString(str, num) {
+  let truncate = '';
+  if (str.length > num) {
+    truncate += str.slice(0, num) + ' ...';
+  } else {
+    truncate += str;
+  }
+  return truncate;
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("Alaje", 8));
