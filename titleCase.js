e0a9b8a4721fb07bf7949f240c176820f53e841c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-04 09:03:58 +0100

    Solve titleCase challenge

---
+++ titleCase.js
@@ -0,0 +1,11 @@
+const titleCase = (str) => {
+  let splittedWord = str.split(' ');
+  let finalSentence = '';
+  for (let i = 0; i < splittedWord.length; i++) {
+    let titleCase = splittedWord[i][0].toUpperCase();
+    finalSentence += titleCase + splittedWord[i].slice(1) + ' '
+  }
+  return finalSentence;
+}
+
+console.log(titleCase("I'm a little tea pot"));
