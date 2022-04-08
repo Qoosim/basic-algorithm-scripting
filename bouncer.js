db2fa776be695fc65a53471e0d2e119bbc7a3f4f
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-08 10:36:57 +0100

    Refactor remove falsy values challenge

--- bouncer.js
+++ bouncer.js
@@ -7,3 +7,9 @@ const bouncer = (arr) => {
 }
 
 console.log(bouncer([7, "ate", "", false, 9]));
+
+function bouncerRefactor(arr) {
+  return arr.filter((elem) => Boolean(elem) === true);
+}
+
+console.log(bouncerRefactor([0, 1, true, "new",  "ate", "", false, 9]));
