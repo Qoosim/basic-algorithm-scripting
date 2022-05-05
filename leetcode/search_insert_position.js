fb9b5af5bf3ea8d6be1e42624f9f8e2d5daddc5b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-05 10:10:13 +0100

    Add a shorter solution to the search insert position challenge

--- leetcode/search_insert_position.js
+++ leetcode/search_insert_position.js
@@ -14,6 +14,15 @@ const searchInsert = (nums, target) => {
   return indexTarget;
 }
 
+
 console.log(searchInsert([1,3,5,6], 5));
 console.log(searchInsert([1,3,5,6], 2));
 console.log(searchInsert([1,3,5,6], 7));
+
+var searchInsert2 = function(nums, target) {
+  return nums.indexOf(target) !== -1 ? nums.indexOf(target) : ([...nums, target].sort((a, b) => a - b)).indexOf(target);
+};
+
+console.log(searchInsert2([1,3,5,6], 5));
+console.log(searchInsert2([1,3,5,6], 2));
+console.log(searchInsert2([1,3,5,6], 7));
