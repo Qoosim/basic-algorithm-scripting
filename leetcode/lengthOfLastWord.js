4165cc1bd4e2dcbed4ae377b8db8da1943572d61
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-05-09 09:51:05 +0100

    Solve length of last word challenge

--- leetcode/lengthOfLastWord.js
+++ leetcode/lengthOfLastWord.js
@@ -14,3 +14,4 @@ console.log(lengthOfLastWord("Hello World"));
 console.log(lengthOfLastWord("   fly me   to   the moon  "));
 console.log(lengthOfLastWord("luffy is still joyboy"));
 
+//  return s.split(' ').reduce((acc, i) => i ? i : acc).length // One line sotution
