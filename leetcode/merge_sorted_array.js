da05329595993429fd2703ac87b54d7dcb294ecf
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-06-13 09:37:34 +0100

    solve merge_sorted_arrays

--- leetcode/merge_sorted_array.js
+++ leetcode/merge_sorted_array.js
@@ -1,6 +1,10 @@
 const mergeSortedArray = (arr1, m, arr2, n) => {
-  return [...arr1,...arr2]
-  return [...arr1].concat(...arr2);
+  let cnt = 0;
+  for (let i = m; i < arr1.length; i++) {
+    arr1[i] = arr2[cnt];
+    cnt++;
+  }
+  return arr1.sort((a, b) => a - b);
 }
 
 console.log(mergeSortedArray([1, 2, 3, 0, 0, 0,], [3], [2, 4, 5], [3]));
