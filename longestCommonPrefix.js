81baca4c4f14ccedf97d47bf95a54c65a7836f4a
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-27 09:49:29 +0000

    Solve the longest common prefix challenge in leetcode platform

---
+++ longestCommonPrefix.js
@@ -0,0 +1,18 @@
+const longestCommonPrefix = function(strs) {
+  let firstElem = strs[0];
+  let leftStrs = strs.splice(1);
+  let result = "";
+  leftStrs.map((str) => {
+    for (let i = 0; i < firstElem.length; i++) {
+      if (str[i] === firstElem[i]) {
+        result += firstElem[i];
+      } else break;
+    }
+    firstElem = result;
+    result = "";
+  })
+  return firstElem;
+}
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+console.log(longestCommonPrefix(["dog","racecar","car"]));
