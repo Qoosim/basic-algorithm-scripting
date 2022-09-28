bc12c3126fa251bf4a8a79af9ecb68b3507ee186
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-28 11:13:45 +0000

    Re-practice the longest common prefix challenge in leetcode platform

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,15 +1,15 @@
 const longestCommonPrefix = function(strs) {
   let firstElem = strs[0];
   let leftStrs = strs.splice(1);
-  let result = "";
-  leftStrs.map((str) => {
+  let result = '';
+  leftStrs.forEach((str) => {
     for (let i = 0; i < firstElem.length; i++) {
       if (str[i] === firstElem[i]) {
         result += firstElem[i];
       } else break;
     }
     firstElem = result;
-    result = "";
+    result = '';
   })
   return firstElem;
 }
