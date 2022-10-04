84d4f379632ef009a7a238d2c9d126e38f7c1f6b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-10-04 13:49:00 +0100

    Delete the longestcommonprefix challenge solution

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,18 +1,2 @@
-const longestCommonPrefix = function(strs) {
-  let firstElem = strs[0];
-  let leftStrs = strs.splice(1);
-  let result = '';
-  leftStrs.forEach((str) => {
-    for (let i = 0; i < firstElem.length; i++) {
-      if (str[i] === firstElem[i]) {
-        result += firstElem[i];
-      } else break;
-    }
-    firstElem = result;
-    result = '';
-  })
-  return firstElem;
-}
-
 console.log(longestCommonPrefix(["flower","flow","flight"]));
 console.log(longestCommonPrefix(["dog","racecar","car"]));
