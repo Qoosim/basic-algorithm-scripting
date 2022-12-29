086f3c55e1ab2b03caa72f4469a91b0723d39664
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-12-29 15:05:23 +0100

    Solve find two arrays intersection with another method

--- leetcode/arraysIntersection.js
+++ leetcode/arraysIntersection.js
@@ -9,3 +9,21 @@ const arraysIntersection = (arr1, arr2) => {
 
 console.log(arraysIntersection([1,2,2,1], [2, 2]));
 console.log(arraysIntersection([4, 9, 5], [9, 4, 9, 8, 4]));
+
+// Another solution
+const arraysIntersection2 = (arr1, arr2) => {
+  const set = new Set([...arr1]);
+  const res = new Set();
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (set.has(arr2[i])) {
+      res.add(arr2[i]);
+    }
+  }
+
+  return [...res];
+}
+
+console.log(arraysIntersection2([1,2,2,1], [2, 2]));
+console.log(arraysIntersection2([4, 9, 5], [9, 4, 9, 8, 4]));
+
