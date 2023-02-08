6bf059e49ec44b2abc22cb868a4edc873eda5e1b
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-02-08 09:19:25 +0100

    Refactor the simple pig latin coding challenge from codeWars platform

--- codeWars/simplePigLatin.js
+++ codeWars/simplePigLatin.js
@@ -1,3 +1,8 @@
+/*
+ Move the first letter of each word to the end of it, then add "ay" to the end of the word. 
+ Leave punctuation marks untouched.
+*/
+
 const simplePigLatin = (str) => {
   const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
   return str.split(' ')
@@ -14,3 +19,14 @@ console.log(simplePigLatin('Pig latin is cool')) // 'igPay atinlay siay oolcay'
 console.log(simplePigLatin('This is my string')) // 'hisTay siay ymay tringsay'
 console.log(simplePigLatin('Hello world !')) // elloHay orldway !
 console.log(simplePigLatin('Quis custodiet ipsos custodes ?')) 
+
+const pigIt = (str) => {
+  //const regex = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+  return str.replace(/\w+/g, (word) => {
+    return word.slice(1) + word.charAt(0) + 'ay';
+  })
+}
+
+console.log(pigIt('This is my second approach'));
+console.log(pigIt('Second method to test it !'));
+console.log(pigIt('Be the best at your professional !'));
