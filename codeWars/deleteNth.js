465272645dd205b00d7e20eefdd87101cf1c4380
Qoosim <ayinde.abdulghaniyy@gmail.com>
2023-06-21 09:24:17 +0100

    Solve getChar challenge at codewars platform

---
+++ codeWars/deleteNth.js
@@ -0,0 +1,10 @@
+const deleteNth = (arr, n) => {
+ for (let i = 0; i < arr.length; i++) {
+  
+ }
+}
+
+console.log(deleteNth([20,37,20,21], 1));
+console.log(deleteNth([1,1,3,3,7,2,2,2,2], 3));
+console.log(deleteNth([1,2,3,1,2,1,2,3], 2));
+
---
+++ codeWars/getChar.js
@@ -0,0 +1,15 @@
+const getChar = () => {
+  let str = "A bird at hand";
+  for (let i = 0; i < str.length; i++) {
+    let ascii = str.charCodeAt(i);
+    console.log(ascii);
+  }
+}
+
+getChar();
+
+function findCorValue(c) {
+  return String.fromCharCode(c); // this returns corresponding value of the char
+}
+
+console.log(findCorValue(65));
