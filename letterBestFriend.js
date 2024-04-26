08b542d3258bf377eeaa265a30a999660646d708
Qoosim <ayinde.abdulghaniyy@gmail.com>
2024-04-26 11:09:01 +0100

    Commit letterBestFriend challenge

---
+++ letterBestFriend.js
@@ -0,0 +1,11 @@
+const bestFriend = (text, a, b) => {
+  const str = text.spilt(" ");
+  return str
+}
+
+console.log(bestFriend("zh", "z", "h"))
+console.log(bestFriend("zoo wee mama", "m", "a"))
+console.log(bestFriend("i found an ounce with my hound", "o", "u"))
+console.log(bestFriend("he headed to the store", "h", "e"))
+console.log(bestFriend("those were their thorns they said", "t", "h"))
+
